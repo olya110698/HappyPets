e8e91560b2cc583b5811e9699bd6feb93fbab74a
olya110698 <78224530+olya110698@users.noreply.github.com>
2022-05-12 19:49:46 +0200

    feat: add part for Marina form

---
+++ happy_pets/src/components/ContainerForForm/ContainerForForm.js
@@ -0,0 +1,43 @@
+import React from "react";
+import styled from "styled-components";
+import Container from "@mui/material/Container";
+
+const css = `
+.container_background_img{
+    position: relative;
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+    justify-content: space-between;
+    background-image: url(../img/background_for_form.jpg);
+    background-size: cover;
+    background-position: unset;
+    background-repeat: no-repeat;
+    height: 698px;
+}
+.css-m2fw4i-MuiContainer-root {
+    padding-left: 0px;
+    padding-right: 0px;
+}`;
+
+const BackgroundForm = styled.div`
+  position: absolute;
+  width: 100%;
+  border-radius: 30px 0 0 30px;
+  -webkit-backdrop-filter: blur(50px);
+  backdrop-filter: blur(50px);
+  top: 0;
+  left: 0px;
+  width: calc(100% - 900px);
+  height: 100%;
+  background: rgba(0, 0, 0, 0.01);
+`;
+
+export default function ContainerForForm() {
+  return (
+    <Container className="container_background_img" maxWidth="auto">
+      <style type="text/css">{css}</style>
+      <BackgroundForm />
+    </Container>
+  );
+}
--- happy_pets/src/pages/StartPage.js
+++ happy_pets/src/pages/StartPage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 import HeaderApp from "../components/HeaderApp/HeaderApp";
+import ContainerForForm from "../components/ContainerForForm/ContainerForForm";
 
 const WrapperContainer = styled.div`
   display: flex;
   align-items: center;
+  flex-direction: column;
   justify-content: center;
   width: 100%;
   height: auto;
@@ -14,6 +16,7 @@ export default function StartPage() {
   return (
     <WrapperContainer>
       <HeaderApp />
+      <ContainerForForm />
     </WrapperContainer>
   );
 }
