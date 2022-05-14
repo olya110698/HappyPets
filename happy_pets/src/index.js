396e4039cdf58ac58f4a288d3166365e2cc682ec
olya110698 <78224530+olya110698@users.noreply.github.com>
2022-05-14 15:57:46 +0200

    feat: change version React

--- happy_pets/src/index.js
+++ happy_pets/src/index.js
@@ -1,16 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <BrowserRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </BrowserRouter>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
-
 reportWebVitals();
